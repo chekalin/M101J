5f36f8804142807365b22fb3dae6aeba7a772c23
Stanislav Chekalin <Stanislavs.Cekalins@Capita.co.uk>
2014-06-05 17:26:25 +0100

    removed example, added combined example

--- src/main/java/com/tengen/HelloWorldMongoDbSparkFreemarkerStyle.java
+++ src/main/java/com/tengen/HelloWorldMongoDbSparkFreemarkerStyle.java
@@ -1,5 +1,6 @@
 package com.tengen;
 
+import com.mongodb.*;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -10,25 +11,23 @@ import spark.Spark;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
 
-public class HelloWorldSparkFreemarkerStyle {
+public class HelloWorldMongoDbSparkFreemarkerStyle {
 
     public static void main(String[] args) throws IOException, TemplateException {
         final Configuration configuration = new Configuration();
-        configuration.setClassForTemplateLoading(HelloWorldSparkFreemarkerStyle.class, "/");
-
-
+        configuration.setClassForTemplateLoading(HelloWorldMongoDbSparkFreemarkerStyle.class, "/");
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
+        final DB database = client.getDB("course");
+        final DBCollection collection = database.getCollection("hello");
         Spark.get(new Route("/") {
             @Override
             public Object handle(final Request request, final Response response) {
                 final StringWriter stringWriter = new StringWriter();
                 try {
                     Template helloTemplate = configuration.getTemplate("hello.ftl");
-                    final Map<String, Object> helloMap = new HashMap<String, Object>();
-                    helloMap.put("name", "Stas");
-                    helloTemplate.process(helloMap, stringWriter);
+                    final DBObject document = collection.findOne();
+                    helloTemplate.process(document, stringWriter);
                 } catch (Exception e) {
                     halt(500);
                 }
